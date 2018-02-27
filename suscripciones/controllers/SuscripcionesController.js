f8796264b3e2424556d2f277c1b44853eef2d772
Joshua Saucedo <joshuansu0897@gmail.com>
2018-02-27 10:45:09 -0700

    seberia funcionar eso de la consulta http, pero algo no jala, no se si es por docker o por el codigo

--- suscripciones/controllers/SuscripcionesController.js
+++ suscripciones/controllers/SuscripcionesController.js
@@ -6,6 +6,9 @@ const Suscripcion = require('../models/Suscripcion');
 // Inicio para la Creacion de las rutas
 const router = express.Router();
 
+// importando nuestro httpRequest
+const rest = require('../util/httpRequest')
+
 // middleware para todos los request
 router.use(function (req, res, next) {
     console.log('algo se anda procesando.');
@@ -17,15 +20,52 @@ router.get('/', function (req, res) {
     res.json({ message: 'Estas en la API del microservicio de Suscripciones' });
 });
 
+let options = {};
+
 // ruta /suscripciones incluye getAll suscripciones y para crear nuevos suscripciones
 router.route('/suscripciones')
 
-    // crea un cliente (POST http://localhost:${PORT}/api/suscripciones)
+    // crea una suscripcion (POST http://localhost:${PORT}/api/suscripciones)
     .post(function (req, res) {
 
-        var suscripcion = new Suscripcion();
-        suscripcion.cliente_id = req.body.cliente_id;
-        suscripcion.servicio_id = req.body.servicio_id;
+
+        let suscripcion = new Suscripcion();
+
+        options = {
+            host: 'localhost',
+            port: 3000,
+            path: `/api/clientes/${req.body.cliente_id}`,
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        rest.getJSON(options, function (statusCode, result) {
+            console.log(statusCode)
+            if (statusCode) {
+                suscripcion.cliente_id = req.body.cliente_id;
+            }
+        });
+
+        options = {
+            hostname: 'http://localhost',
+            port: 3333,
+            path: `/api/servicios/${req.body.servicio_id}`,
+            method: 'GET'
+        };
+
+        rest.getJSON(options, function (statusCode, result) {
+            console.log(statusCode)
+            if (statusCode) {
+                suscripcion.servicio_id = req.body.servicio_id;
+            }
+        });
+
+        if (suscripcion.servicio_id == undefined || suscripcion.cliente_id == undefined) {
+            res.json({ message: 'No se pudo crear la suscripcion' });
+            return;
+        }
 
         suscripcion.save(function (err) {
             if (err) {
